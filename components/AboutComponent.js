6a4d129e5e8c2727f3af14df2bbfd95f27d8ea9f
Sayed <sadatsayed92@gmail.com>
2020-01-06 23:09:58 -0800

    Redux Adding Favorites

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,14 +3,13 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
       partners: state.partners
     };
 };
-
-
 
 function Mission() {
     return(
@@ -33,27 +32,47 @@ class About extends Component {
          title: 'About Us'
      };
 
-
-    render() {
+     render() {
         const renderPartner = ({item}) => {
-            return(
+            return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
-        } 
+        };
 
-        return(
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        return (
             <ScrollView>
-                <Mission
-                    item={this.state.partners.filter(partner => partner.featured)[0]}/>
+                <Mission />
                 <Card title="Community Partners">
                 <FlatList 
                     data={this.props.partners.partners}
                     renderItem={renderPartner}
-                    keyExtractor={item => item.id.toString()}
+                    keyExtractor={item=>item.id.toString()}
                 />
                 </Card>
 
